f8c6292a220ce054b615d42b238449651c0d215e
Manya Nigam <nigammanya5@gmail.com>
2026-05-01 20:30:15 +0530

    riskprediction

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Home';
 import SmartSummarization from './SmartSummarization';
 import DrugInteractions from "./DrugInteractions";
+import PatientRiskScore from "./PatientRiskScore";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         {/* The new smart summarization page */}
         <Route path="/smart-summarization" element={<SmartSummarization />} />
         <Route path="/drug-interactions" element={<DrugInteractions />} />
+        <Route path="/risk-prediction" element={<PatientRiskScore />} />
       </Routes>
     </Router>
   );
